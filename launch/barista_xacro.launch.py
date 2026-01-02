1d335a50d6b9dffc7670e1713cb7d35ffe210c54
Alok <alok@sourceinfotech.com>
2026-01-02 13:26:55 +0000

    CP08 Part 1: Multiple robots with namespaces

---
+++ launch/barista_xacro.launch.py
@@ -0,0 +1,21 @@
+from launch import LaunchDescription
+from launch_ros.actions import Node
+from launch.substitutions import Command
+from ament_index_python.packages import get_package_share_directory
+import os
+
+def generate_launch_description():
+
+    pkg = get_package_share_directory('barista_robot_description')
+    xacro_file = os.path.join(pkg, 'xacro', 'barista_robot_model.urdf.xacro')
+
+    robot_description = Command(['xacro ', xacro_file])
+
+    return LaunchDescription([
+        Node(
+            package='robot_state_publisher',
+            executable='robot_state_publisher',
+            parameters=[{'robot_description': robot_description}],
+            output='screen'
+        )
+    ])
